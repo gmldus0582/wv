0218a5836c1b65c57b921747361ac122c56f180f
DESKTOP-BTVUULG\User <gmldus0582@naver.com>
2021-02-02 10:37:58 +0900

    barcode scanner 구현

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import { WebView } from 'react-native-webview';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
 
-class MyWeb extends Component {
+class url extends Component {
   render() {
     return (
       <WebView
@@ -11,7 +13,30 @@ class MyWeb extends Component {
     );
   }
 }
+  const barcode = () =>{
+    return (
+      <QRCodeScanner
+        onRead={this.onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        // topContent={
+        //   <Text style={styles.centerText}>
+        //     Go to{' '}
+        //     <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+        //     your computer and scan the QR code.
+        //   </Text>
+        // }
+        // bottomContent={
+        //   <TouchableOpacity style={styles.buttonTouchable}>
+        //     <Text style={styles.buttonText}>OK. Got it!</Text>
+        //   </TouchableOpacity>
+        // }
+      />
+    );
+    
+  }
 
-
-
-export default MyWeb;
+  onSuccess = e => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err)
+    );}
+export default barcode;
